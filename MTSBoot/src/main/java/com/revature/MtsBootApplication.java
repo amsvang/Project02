7fc7e38d058ecc9b4479615deb6e90db6a841d59
Amy V <amyvangsinbox@outlook.com>
2022-05-23 22:22:12 -0400

    setup h2 database

--- MTSBoot/src/main/java/com/revature/MtsBootApplication.java
+++ MTSBoot/src/main/java/com/revature/MtsBootApplication.java
@@ -2,13 +2,18 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication()
 public class MtsBootApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(MtsBootApplication.class, args);
+//		SpringApplication.run(MtsBootApplication.class, args);
+
+		ConfigurableApplicationContext configurableApplicationContext =
+			SpringApplication.run(MtsBootApplication.class, args);
+
 
 	}
 }
